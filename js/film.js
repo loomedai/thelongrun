dc288e58d083bd8c34f3a0e2c5ecb45f081c74c4
loomedai <bbrith@hotmail.com>
2021-10-10 13:24:11 +0200

    js og api

---
+++ js/film.js
@@ -0,0 +1,67 @@
+export default class Film {
+    constructor() {
+        this.data = {
+            password:"Ghibli"
+        }
+        this.rootElem = document.querySelector('.film');
+        this.filter = this.rootElem.querySelector('.filter');
+        this.items = this.rootElem.querySelector('.items');
+        this.nameSearch = this.filter.querySelector('.nameSearch');
+
+    }
+
+    async init(){
+        this.nameSearch.addEventListener('input', ()=>{
+            if(this.nameSearch.value.length >= 2){
+                this.render()
+            }
+        });
+
+        await this.render();
+
+    }
+
+    async render(){
+        const data = await this.getData();
+        console.log(data);
+
+
+        const row = document.createElement('div');
+        row.classList.add('row', 'g-4');
+
+        for(const item of data){
+            const col = document.createElement('div');
+            col.classList.add('col-md-6', 'col-lg-4', 'col-xl-3');
+
+            col.innerHTML = `
+                <div class="card">
+                    <img src="uploads/${item.prodPhoto}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-titel">${item.prodTitel}</h5>
+                        <p class="card-text">${item.prodDes}</p>
+                        <a href="" class="btn btn-primary text-white w-100">Læs mere</a>
+</div>
+                
+                </div>
+                
+        `;
+            row.appendChild(col);
+
+        }
+        this.items.innerHTML = '';
+        this.items.appendChild(row);
+
+    }
+
+    async getData(){
+        this.data.nameSearch = this.nameSearch.value;
+
+        const response = await fetch('api.php', {
+            method: "POST",
+            body:JSON.stringify(this.data)
+        });
+        return await response.json();
+
+
+    }
+}
