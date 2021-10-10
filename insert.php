5149251315ce689f22787258f54bac541180362a
loomedai <bbrith@hotmail.com>
2021-10-10 14:22:55 +0200

    1. nav

--- insert.php
+++ insert.php
@@ -46,11 +46,39 @@ if(!empty($_POST["data"])){
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.scss" rel="stylesheet" type="text/css">
 
-    <img src="images/totoro2.png" id="totorophoto">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <script src="https://cdn.tiny.cloud/1/y7etbb6fgqruwg5o86t8eo2wywvvdlu54udfhebfjweavmfg/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="insert.php">Ghibli data</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Library
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="arrietty.php">Arrietty</a></li>
+                            <li><a class="dropdown-item" href="ponyo.php">Ponyo</a></li>
+                            <li><hr class="dropdown-item" href="#">My neighbor Totoro</li>
+                            <li><a class="dropdown-item" href="#">Howls moving castle</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <img src="images/totoro2.png" id="totorophoto">
 </head>
 
 <body>
